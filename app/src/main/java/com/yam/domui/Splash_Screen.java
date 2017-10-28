8a56149912fd66e833436f9eae4132ca59dee298
Beto Mina <yuberalberto@gmail.com>
2017-10-28 11:09:49 -0500

    Mina Prueba
    prueba

---
+++ app/src/main/java/com/yam/domui/Splash_Screen.java
@@ -0,0 +1,26 @@
+package com.yam.domui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Splash_Screen.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        },2000);
+    }
+}
